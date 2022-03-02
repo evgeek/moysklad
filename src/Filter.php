61dab7c89794b301c9a80aedb9ff02852ff69691
evgeek <evgeeek@gmail.com>
2022-03-02 13:50:21 +0300

    Adds comments to Filter methods

--- src/Filter.php
+++ src/Filter.php
@@ -18,54 +18,63 @@ class Filter
         return $this->filters;
     }
 
+    /** Equal (=) */
     public function eq(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::EQ);
         return $this;
     }
 
+    /** Not equal (!=) */
     public function neq(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::NEQ);
         return $this;
     }
 
+    /** Greater than (>) */
     public function gt(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::GT);
         return $this;
     }
 
+    /** Lower than (<) */
     public function lt(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::LT);
         return $this;
     }
 
+    /** Greater than or equal (>=) */
     public function gte(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::GTE);
         return $this;
     }
 
+    /** Lower than or equal (<=) */
     public function lte(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::LTE);
         return $this;
     }
 
+    /** Like (~) */
     public function like(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::LIKE);
         return $this;
     }
 
+    /** Like by prefix (~=) */
     public function prefix(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::PREFIX);
         return $this;
     }
 
+    /** Like by postfix (~=) */
     public function postfix(string $key, string $value): static
     {
         $this->addFilter($key, $value, FilterSigns::POSTFIX);
