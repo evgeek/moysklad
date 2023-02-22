4b1712903d78eaf20b8baf78065f2bcf2d3575d1
evgeek <evgeeek@gmail.com>
2023-02-22 11:43:48 +0300

    QueryParamTest

---
+++ tests/Unit/Enums/QueryParamTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Evgeek\Tests\Unit\Enums;
+
+use Evgeek\Moysklad\Enums\QueryParam;
+use PHPUnit\Framework\TestCase;
+
+/** @covers \Evgeek\Moysklad\Enums\QueryParam */
+class QueryParamTest extends TestCase
+{
+    /** @dataProvider separatorDataProvider */
+    public function testSeparator(QueryParam $queryParam, string $separator): void
+    {
+        $this->assertEquals($separator, $queryParam->separator());
+    }
+
+    /** @dataProvider getSeparatorDataProvider */
+    public function testGetSeparator(QueryParam|string $queryParam, string $separator): void
+    {
+        $this->assertEquals($separator, QueryParam::getSeparator($queryParam));
+    }
+
+    private function separatorDataProvider(): array
+    {
+        return [
+            [QueryParam::EXPAND, ','],
+            [QueryParam::FILTER, ';'],
+            [QueryParam::ORDER, ';'],
+            [QueryParam::LIMIT, ''],
+            [QueryParam::OFFSET, ''],
+            [QueryParam::SEARCH, ''],
+        ];
+    }
+
+    private function getSeparatorDataProvider()
+    {
+        return array_merge($this->separatorDataProvider(), [
+            ['expand', ','],
+            ['FILTER', ';'],
+            ['OrdEr', ';'],
+            ['limit', ''],
+            ['offset', ''],
+            ['search', ''],
+            ['unknown', ''],
+        ]);
+    }
+}
