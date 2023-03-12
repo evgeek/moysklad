40dff334ef8086bd3c13b485888c02db636edfca
evgeek <evgeeek@gmail.com>
2023-03-12 18:50:20 +0300

    move crud to trait

---
+++ src/ApiObjects/Collections/Traits/CrudCollectionTrait.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evgeek\Moysklad\ApiObjects\Collections\Traits;
+
+use Evgeek\Moysklad\Enums\HttpMethod;
+use Evgeek\Moysklad\Exceptions\RequestException;
+use Evgeek\Moysklad\Http\Payload;
+use Evgeek\Moysklad\Services\Url;
+use InvalidArgumentException;
+
+trait CrudCollectionTrait
+{
+    /**
+     * @throws RequestException
+     */
+    public function get(): static
+    {
+        return $this->send(HttpMethod::GET);
+    }
+
+    /**
+     * @throws RequestException
+     */
+    public function massCreateUpdate(array $objects): static
+    {
+        return $this->sendAndWrapResponse(HttpMethod::POST, $objects);
+    }
+
+    /**
+     * @throws RequestException
+     */
+    public function massDelete(array $objects): static
+    {
+        return $this->sendAndWrapResponse(HttpMethod::POST, $objects, 'delete');
+    }
+
+    /**
+     * @throws RequestException
+     */
+    protected function send(HttpMethod $method, mixed $body = []): static
+    {
+        $payload = $this->makePayload($method, $body);
+
+        $response = $this->ms->getApiClient()->send($payload);
+        $this->hydrate($response);
+
+        return $this;
+    }
+
+    /**
+     * @throws RequestException
+     */
+    protected function sendAndWrapResponse(HttpMethod $method, mixed $body, ?string $additionalSegment = null): static
+    {
+        $response = [];
+        $meta = $this->meta ?? null;
+        $context = $this->context ?? null;
+        if ($meta) {
+            $response['meta'] = $meta;
+        }
+        if ($context) {
+            $response['context'] = $context;
+        }
+
+        $payload = $this->makePayload($method, $body, $additionalSegment);
+        $response['rows'] = $this->ms->getApiClient()->send($payload);
+
+        $this->hydrate($response);
+
+        return $this;
+    }
+
+    protected function makePayload(HttpMethod $method, mixed $body, ?string $additionalSegment = null): Payload
+    {
+        $href = $this->meta->href ?? null;
+        if (!$href) {
+            throw new InvalidArgumentException('Cannot find meta href');
+        }
+
+        [$path, $params] = Url::parsePathAndParams($href);
+        $path = $additionalSegment ? [...$path, $additionalSegment] : $path;
+
+        return new Payload(
+            method: $method,
+            path: $path,
+            params: $params,
+            body: $body,
+        );
+    }
+}
--- src/ApiObjects/Collections/UnknownCollection.php
+++ src/ApiObjects/Collections/UnknownCollection.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Evgeek\Moysklad\ApiObjects\Collections;
 
 use Evgeek\Moysklad\ApiObjects\AbstractUnknownApiObject;
+use Evgeek\Moysklad\ApiObjects\Collections\Traits\CrudCollectionTrait;
 use Evgeek\Moysklad\ApiObjects\Collections\Traits\FillMetaCollectionTrait;
 
 class UnknownCollection extends AbstractUnknownApiObject
 {
+    use CrudCollectionTrait;
     use FillMetaCollectionTrait;
 }
