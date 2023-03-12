051996f74385d46f5ad0d069f347b3b5013a0f0a
evgeek <evgeeek@gmail.com>
2023-03-12 15:58:50 +0300

    hide meta in objects without id

--- src/ApiObjects/Objects/AbstractConcreteObject.php
+++ src/ApiObjects/Objects/AbstractConcreteObject.php
@@ -8,10 +8,24 @@ use Evgeek\Moysklad\ApiObjects\AbstractConcreteApiObject;
 use Evgeek\Moysklad\ApiObjects\Objects\Traits\CrudObjectTrait;
 use Evgeek\Moysklad\ApiObjects\Objects\Traits\SetIdInMetaHrefTrait;
 use Evgeek\Moysklad\ApiObjects\Objects\Traits\SetMetaObjectTrait;
+use Evgeek\Moysklad\Formatters\AbstractMultiDecoder;
+use Evgeek\Moysklad\Services\Url;
 
 abstract class AbstractConcreteObject extends AbstractConcreteApiObject
 {
     use CrudObjectTrait;
     use SetIdInMetaHrefTrait;
     use SetMetaObjectTrait;
+
+    public function toArray(): array
+    {
+        $container = $this->contentContainer;
+        if ($this->meta && !Url::getId($this->meta->href)) {
+            unset($container['meta']);
+
+            return AbstractMultiDecoder::toArray($container);
+        }
+
+        return AbstractMultiDecoder::toArray($container);
+    }
 }
