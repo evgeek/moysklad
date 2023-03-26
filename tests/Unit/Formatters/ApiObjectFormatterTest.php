cfbf856e1f0ff8b48fe27e337a93d028047b864e
evgeek <evgeeek@gmail.com>
2023-03-26 19:01:40 +0300

    test

--- tests/Unit/Formatters/ApiObjectFormatterTest.php
+++ tests/Unit/Formatters/ApiObjectFormatterTest.php
@@ -6,13 +6,12 @@ use Evgeek\Moysklad\ApiObjects\AbstractApiObject;
 use Evgeek\Moysklad\ApiObjects\Collections\ProductCollection;
 use Evgeek\Moysklad\ApiObjects\Objects\Employee;
 use Evgeek\Moysklad\Formatters\ApiObjectFormatter;
-use stdClass;
 
 /**
  * @covers \Evgeek\Moysklad\Formatters\AbstractMultiDecoder
  * @covers \Evgeek\Moysklad\Formatters\ApiObjectFormatter
  */
-class ApiObjectFormatterTest extends MultiDecoderTestCase
+class ApiObjectFormatterTest extends StdClassFormatTest
 {
     protected const FORMATTER = ApiObjectFormatter::class;
 
@@ -32,55 +31,5 @@ class ApiObjectFormatterTest extends MultiDecoderTestCase
         }
 
         $this->assertSame($formattedCasted, $resultFormatted);
-    }
-
-    protected static function getEncodedObject(): stdClass
-    {
-        return (object) [
-            'param' => 'test_param',
-            'meta' => (object) [
-                'href' => 'https://online.moysklad.ru/api/remap/1.2/endpoint/segment',
-                'type' => 'product',
-            ],
-            'context' => (object) [
-                'employee' => (object) [
-                    'meta' => (object) [
-                        'href' => 'https://online.moysklad.ru/api/remap/1.2/context/employee',
-                        'type' => 'employee',
-                    ],
-                ],
-            ],
-            'rows' => [
-                (object) [
-                    'id' => 'id1',
-                    'value' => true,
-                ],
-                (object) [
-                    'id' => 'id2',
-                    'value' => 0,
-                ],
-                (object) [
-                    'id' => 'id3',
-                    'value' => null,
-                ],
-                (object) [
-                    'id' => 'id4',
-                    'value' => 123.45,
-                ],
-            ],
-        ];
-    }
-
-    protected static function getEncodedArray(): array
-    {
-        return [
-            (object) ['param' => 'value1', 'meta' => 'meta1'],
-            (object) ['param' => 'value2', 'meta' => 'meta2'],
-        ];
-    }
-
-    protected static function getEncodedEmpty(): stdClass
-    {
-        return new stdClass();
     }
 }
