82dfc818ab87e4b3fa877ccedb96d0c56bef2c59
evgeek <evgeeek@gmail.com>
2023-04-09 13:47:10 +0300

    ParamsObjectTraitTest

---
+++ tests/Unit/ApiObjects/Objects/Traits/ParamsObjectTraitTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evgeek\Tests\Unit\ApiObjects\Objects\Traits;
+
+use Evgeek\Moysklad\Services\Url;
+
+/**
+ * @covers \Evgeek\Moysklad\ApiObjects\Objects\Traits\ParamsObjectTrait
+ */
+class ParamsObjectTraitTest extends ObjectTraitCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->expectedUrl = Url::API . '/' . implode('/', static::PATH);
+    }
+
+    public function testSingleExpandMethod(): void
+    {
+        $object = $this->getTestObject([])
+            ->expand('field1');
+        $this->expectedUrl .= '?expand=field1';
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testMultipleExpandMethods(): void
+    {
+        $object = $this->getTestObject([])
+            ->expand('field1')
+            ->expand('field2');
+        $this->expectedUrl .= '?expand=' . urlencode('field1,field2');
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testExpandMethodWithMultipleExpands(): void
+    {
+        $object = $this->getTestObject([])
+            ->expand(['field1', 'field2']);
+        $this->expectedUrl .= '?expand=' . urlencode('field1,field2');
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testSingleParamMethod(): void
+    {
+        $object = $this->getTestObject([])
+            ->param('field', 'value');
+        $this->expectedUrl .= '?field=value';
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testMultipleParamMethodsWithReplacedParam(): void
+    {
+        $object = $this->getTestObject([])
+            ->param('field', 'value1')
+            ->param('field', 'value2');
+        $this->expectedUrl .= '?field=value2';
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testMultipleParamMethodsWithAddedParam(): void
+    {
+        $object = $this->getTestObject([])
+            ->param('filter', 'key1=value1')
+            ->param('filter', 'key2=value2');
+        $this->expectedUrl .= '?filter=' . urlencode('key1=value1;key2=value2');
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+
+    public function testParamMethodsWithMultipleDifferentParams(): void
+    {
+        $object = $this->getTestObject([])
+            ->param([
+                ['filter', 'key1=value1'],
+                ['expand', 'field1'],
+                ['expand', 'field2'],
+                ['param', 'value1'],
+                ['param', 'value2'],
+            ])
+            ->param('filter', 'key2=value2');
+        $this->expectedUrl .= '?filter=' . urlencode('key1=value1;key2=value2') .
+            '&expand=' . urlencode('field1,field2') .
+            '&param=value2';
+
+        $this->assertSame($this->expectedUrl, $object->meta->href);
+    }
+}
