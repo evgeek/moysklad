5959291eb37e5a582d9b990b335e4cae0c293f4b
evgeek <evgeeek@gmail.com>
2023-11-15 21:13:47 +0300

    init

--- src/Api/Query/Segments/ById/AbstractByIdSegment.php
+++ src/Api/Query/Segments/ById/AbstractByIdSegment.php
@@ -7,9 +7,11 @@ namespace Evgeek\Moysklad\Api\Query\Segments\ById;
 use Evgeek\Moysklad\Api\Query\Segments\AbstractCommonSegment;
 use Evgeek\Moysklad\Api\Query\Traits\Actions\DeleteTrait;
 use Evgeek\Moysklad\Api\Query\Traits\Actions\UpdateTrait;
+use Evgeek\Moysklad\Api\Query\Traits\Segments\ById\ByIdCommonTrait;
 
 abstract class AbstractByIdSegment extends AbstractCommonSegment
 {
     use DeleteTrait;
     use UpdateTrait;
+    use ByIdCommonTrait;
 }
